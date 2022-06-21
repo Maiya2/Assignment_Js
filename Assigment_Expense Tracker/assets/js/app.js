549348f1f0067f7fed0d8ac6fe9d36a1d62f243e
Maiya2 <ss5706817@gmail.com>
2022-06-21 20:55:01 +0500

    first

---
+++ Assigment_Expense Tracker/assets/js/app.js
@@ -0,0 +1,26 @@
+const blance = document.getElementById("blance");
+
+const money_plus = document.getElementById("money-plus");
+const money_minus = document.getElementById("money-minus");
+const list = document.getElementById("list");
+const form = document.getElementById("from");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+
+
+const dummyTransctions =[
+    {id: 1, text:"Flower",amount:-20 },
+    {id: 2, text:"salary",amount:-20 },
+    {id: 3, text:"Book",amount:-20 },
+    {id: 4, text:"camera",amount:-20 },
+];
+
+let Transctions = dummyTransctions;
+
+function addTransctionsDOM(Transctions){
+  const sing = 
+
+ }
+
+
+
